717824adfffc26a1407e2bf579a44e9ebad7c1b4
nurkanwal <nooorkanwal@gmail.com>
2025-03-15 07:17:50 +0500

    new file index

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>My log book</h1>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, iure laborum, sapiente odit et explicabo quas vitae facilis vel, expedita veritatis eos dolore. Minus fuga rem fugit ut, et maxime quaerat animi, accusantium saepe dolore, atque doloremque. Facere iure ea minima, non laborum nam, voluptatem culpa atque ratione pariatur minus.
+    </p>
+</body>
+</html>
